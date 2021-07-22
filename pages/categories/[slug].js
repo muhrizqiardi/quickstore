886711642537d647e0d14604a3d0b6fddd103fc8
Muhammad Rizqi Ardiansyah <rizqi.jpg@gmail.com>
2021-07-22 17:38:20 +0700

    removed unnecessary quote

--- pages/categories/[slug].js
+++ pages/categories/[slug].js
@@ -10,7 +10,7 @@ function CategoryPage({ products, category }) {
       <section id="products" className="my-3">
         <div className="container">
           <h3 className="mb-3">
-            Showing "{category.name}"
+            Showing {category.name}
           </h3>
           <div className="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-1 g-3">
             {
